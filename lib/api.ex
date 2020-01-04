e5de6365ec1fb965a49e572751b1b7a04812c8a7
Andrei <eu@andreih.com>
2020-01-04 14:47:53 -0300

    Add support to fetch_models

--- lib/api.ex
+++ lib/api.ex
@@ -1,13 +1,20 @@
 defmodule Fipex.Api do
   def fetch_reference_dates do
-    Fipex.post('ConsultarTabelaDeReferencia', [])
-      |> process_response
+    Fipex.post('ConsultarTabelaDeReferencia', []) |> process_response
   end
 
   def fetch_makes(reference_date_code, vehicle_type) do
-    body = [codigoTabelaReferencia: reference_date_code, codigoTipoVeiculo: vehicle_type]
-    Fipex.post('ConsultarMarcas', {:form, body})
-      |> process_response
+    params = [codigoTabelaReferencia: reference_date_code, codigoTipoVeiculo: vehicle_type]
+    Fipex.post('ConsultarMarcas', {:form, params}) |> process_response
+  end
+
+  def fetch_models(reference_date_code, vehicle_type, make_code) do
+    params = [
+      codigoTabelaReferencia: reference_date_code,
+      codigoTipoVeiculo: vehicle_type,
+      codigoMarca: make_code
+    ]
+    Fipex.post('ConsultarModelos', {:form, params}) |> process_response
   end
 
   defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
--- test/api_test.exs
+++ test/api_test.exs
@@ -6,6 +6,13 @@ defmodule Fipex.ApiTest do
     HTTPoison.start
   end
 
+  @valid_reference_date 250
+  @invalid_reference_date 666
+  @valid_vehicle_type 1
+  @invalid_vehicle_type 66
+  @valid_make_code 23
+  @invalid_make_code 666
+
   test "fetch_reference_dates/0" do
     use_cassette "fetch_reference_dates" do
       {:ok, dates} = Fipex.Api.fetch_reference_dates
@@ -18,17 +25,34 @@ defmodule Fipex.ApiTest do
 
   test "fetch_makes/2 returns a list of makes" do
     use_cassette "fetch_makes" do
-      {:ok, makes} = Fipex.Api.fetch_makes(250, Fipex.vehicle_types[:car])
+      {:ok, makes} = Fipex.Api.fetch_makes(@valid_reference_date, @valid_vehicle_type)
       assert Enum.at(makes, 0) == %{"Label" => "Acura", "Value" => "1"}
     end
   end
 
-  test "fetch_makes/2 fails when a parameter are invalid" do
+  test "fetch_makes/2 fails when a parameter is invalid" do
     use_cassette "fetch_makes_invalid" do
-      {:error, reason} = Fipex.Api.fetch_makes(666, Fipex.vehicle_types[:car])
+      {:error, reason} = Fipex.Api.fetch_makes(@invalid_reference_date, @valid_vehicle_type)
       assert reason == "nadaencontrado"
 
-      {:error, reason} = Fipex.Api.fetch_makes(250, Fipex.vehicle_types[:car] + 66)
+      {:error, reason} = Fipex.Api.fetch_makes(@valid_reference_date, @invalid_vehicle_type)
+      assert reason == "nadaencontrado"
+    end
+  end
+
+  test "fetch_models/3 returns a list of models for a given make" do
+    use_cassette "fetch_models" do
+      {:ok, models} =
+        Fipex.Api.fetch_models(@valid_reference_date, @valid_vehicle_type, @valid_make_code)
+      assert Enum.at(models["Modelos"], 103) ==
+        %{"Label" => "Chevette L / SL / SL/e / DL / SE 1.6", "Value" => 1010}
+    end
+  end
+
+   test "fetch_models/3 fails when a parameter is invalid" do
+    use_cassette "fetch_models_invalid" do
+      {:error, reason} =
+        Fipex.Api.fetch_models(@valid_reference_date, @valid_vehicle_type, @invalid_make_code)
       assert reason == "nadaencontrado"
     end
   end
